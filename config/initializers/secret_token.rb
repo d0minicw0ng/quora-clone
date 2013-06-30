817fde5991c7228e2784223dbbd3c93e969afa31
dominic.wong.617 <dominic.wong.617@gmail.com>
2013-06-29 18:39:03 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quora::Application.config.secret_token = '1b08b8d17055dbcc9f578f6a7847b7e4e9a7e53d3c010e8905088ba4eda9352fbaefc11409501b0608b026b3f87c3b1ccc9007f702a02b8381426290b9c8adcd'
