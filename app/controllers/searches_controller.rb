c08982894c781563c00baad602d1df41e0aeba8c
App Academy Student <student@appacademy.io>
2013-07-05 18:11:53 -0700

    search result style fix

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,6 @@
 class SearchesController < ApplicationController
   def index
     @results = PgSearch.multisearch(params[:query])
+      .paginate(:page => params[:page], :per_page => 10)
   end
 end
