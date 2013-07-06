0d0615839142fc3b6661d3de5d60b4963e09dde2
App Academy Student <student@appacademy.io>
2013-07-06 02:06:43 -0700

    more fix to selecting social feed

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,12 +1,14 @@
 class ActivitiesController < ApplicationController
   def index
     @activities = PublicActivity::Activity.order('created_at DESC')
-
-    @activities.select do |activity|
-      (activity.owner_type.is_a?(User) &&
-        current_user.followed_user_ids.include?(activity.owner_id)) ||
-      (activity.owner_type.is_a?(Question) &&
-        current_user.followed_question_ids.include?(activity.owner_id))
+    @activities = @activities.select do |activity|
+      (activity.owner_type == "User" &&
+        (current_user.followed_user_ids.include?(activity.owner_id)) ||
+          activity.owner_id != current_user.id) ||
+      (activity.owner_type == "Question" &&
+        current_user.followed_question_ids.include?(activity.owner_id)) ||
+      (activity.trackable_type == "Topic" &&
+        current_user.followed_topic_ids.include?(activity.trackable_id))
     end
 
     @topics = Topic.trending_topics[0..5]
