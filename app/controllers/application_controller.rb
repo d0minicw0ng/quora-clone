713f42766f93a557404c2673e5dc3989f6c35e1f
App Academy Student <student@appacademy.io>
2013-07-10 10:40:08 -0700

    access control allow origin *

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 	before_filter :authenticate_user!
+  helper_method :broadcast_server
 
   include PublicActivity::StoreController
+
+  def broadcast_server
+    if request.port.to_i != 80
+      "http://dominic-wong-faye-server.herokuapp.com:80/faye"
+    else
+      "http://dominic-wong-faye-server.herokuapp.com/faye"
+    end
+  end
 end
